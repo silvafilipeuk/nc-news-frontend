8b682f4a8a1b415f6371587ea5652ae4fa35b0e3
Filipe da Silva <silvafilipeuk@gmail.com>
2024-05-07 14:46:41 +0100

    Comments implemented to be displayed alongside an article...

--- src/Components/ArticlesById.jsx
+++ src/Components/ArticlesById.jsx
@@ -3,27 +3,51 @@ import styled from "styled-components";
 import ArticleCard from "./ReusableComponents/ArticleCard";
 import { getArticleById } from "../utils/getArticles";
 import { useEffect, useState } from "react";
+import getComments from "../utils/getComments";
+import CommentCard from "./ReusableComponents/CommentCard";
 
 const Container = styled.div`
 	display: grid;
 `;
 
+const CommentsBox = styled.div`
+	display: grid;
+	justify-self: start;
+	margin: 10px;
+	padding: 5px;
+	font-family: "Roboto", sans-serif;
+	font-weight: 700;
+	font-style: normal;
+`;
+
 function ArticlesById() {
 	const [article, setArticle] = useState([]);
+	const [comments, setComments] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 	const { article_id } = useParams();
 
 	useEffect(() => {
-		Promise.all([getArticleById(article_id)]).then((response) => {
-			setArticle(response[0].article);
-			setIsLoading(false);
-		});
+		Promise.all([getArticleById(article_id), getComments(article_id)]).then(
+			(response) => {
+				setArticle(response[0].article);
+				setComments(response[1].comments);
+				setIsLoading(false);
+			}
+		);
 	}, []);
 
 	return isLoading ? (
 		"Loading"
 	) : (
-		<Container>{<ArticleCard article={article} />}</Container>
+		<Container>
+			<ArticleCard article={article} />
+			<CommentsBox>Comments:</CommentsBox>
+			{comments.map((comment) => {
+				return (
+					<CommentCard key={comment.comment_id} comment={comment} />
+				);
+			})}
+		</Container>
 	);
 }
 
--- src/Components/ReusableComponents/CommentCard.jsx
+++ src/Components/ReusableComponents/CommentCard.jsx
@@ -1,1 +1,64 @@
+import styled from "styled-components";
+import sizes from "../../utils/breakPoints";
+import formatCourseDate from "../../utils/utils";
 
+const CommentBox = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	width: 300px;
+	padding: 10px;
+	margin: 10px;
+	grid-template-areas:
+		"creation_date author"
+		"body body"
+		"votes votes";
+	box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+
+	@media ${sizes.md} {
+		width: 700px;
+	}
+`;
+
+const CreationDate = styled.div`
+	grid-area: creation_date;
+	justify-self: start;
+	padding: 5px 0px 5px 5px;
+`;
+
+const Author = styled.div`
+	grid-area: author;
+	justify-self: end;
+	padding: 5px 5px 5px 0px;
+`;
+
+const Body = styled.div`
+	grid-area: body;
+	padding: 5px 5px 15px 5px;
+`;
+
+const Votes = styled.div`
+	grid-area: votes;
+	justify-self: start;
+	padding: 5px 0px 5px 5px;
+`;
+
+function CommentCard({ comment }) {
+	const date = formatCourseDate(comment.created_at.split("T")[0]);
+	const hour = comment.created_at.split("T")[1].slice(0, 5);
+	return (
+		<CommentBox>
+			<CreationDate>
+				<strong>Date:</strong> {date + " - " + hour}
+			</CreationDate>
+			<Author>
+				<strong>Author:</strong> {comment.author}
+			</Author>
+			<Body>{comment.body}</Body>
+			<Votes>
+				<strong>Votes:</strong> {comment.votes}
+			</Votes>
+		</CommentBox>
+	);
+}
+
+export default CommentCard;
